c1995136aa77bb626f54acd586d662ae4977c2b7
Hank Wang <drapho@gmail.com>
2015-12-27 17:15:01 +0800

    Supported slack command

--- bot-currency.go
+++ bot-currency.go
@@ -43,8 +43,28 @@ func renew_handler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
 }
 
+func slack_handler(w http.ResponseWriter, r *http.Request) {
+	currency, err := info.Get(r.FormValue("text"))
+	response := ""
+	if err != nil {
+		response = "找不到這個貨幣耶"
+	} else {
+		response = "現金買入: "+currency.BuyCach+", 現金賣出: "+currency.SellCash+"\n即期買入: "+currency.BuySpot+", 即期賣出: "+currency.SellSpot
+	}
+	json.NewEncoder(w).Encode(map[string]string{
+	    "response_type": "in_channel",
+	    "text": "Currency rate for: " strings.ToUpper(r.FormValue("text")),
+	    "attachments": [
+	        {
+	            "text": response
+	        }
+	    ]
+	})
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/slack", slack_handler)
 	http.HandleFunc("/renew", renew_handler)
 	log.Fatal(http.ListenAndServe(":3030", Log(http.DefaultServeMux)))
 }
