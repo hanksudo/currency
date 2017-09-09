66374649f71c98a35bd09664beec5eb2da6d8289
hanksudo <drapho@gmail.com>
2017-09-10 02:16:29 +0900

    lint go file

--- bot-currency.go
+++ bot-currency.go
@@ -11,7 +11,7 @@ import (
 	"github.com/hanksudo/bot-currency/info"
 )
 
-func Log(handler http.Handler) http.Handler {
+func logging(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
 		handler.ServeHTTP(w, r)
@@ -38,23 +38,23 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(content)
 }
 
-func renew_handler(w http.ResponseWriter, r *http.Request) {
+func renewHandler(w http.ResponseWriter, r *http.Request) {
 	currency.Renew()
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
 }
 
-type SlackAttachment struct {
+type slackAttachment struct {
 	Text string `json:"text"`
 }
 
-type SlackPostMessage struct {
+type slackPostMessage struct {
 	ResponseType string            `json:"response_type"`
 	Text         string            `json:"text"`
-	Attachments  []SlackAttachment `json:"attachments"`
+	Attachments  []slackAttachment `json:"attachments"`
 }
 
-func slack_handler(w http.ResponseWriter, r *http.Request) {
+func slackHandler(w http.ResponseWriter, r *http.Request) {
 	currency, err := info.Get(r.FormValue("text"))
 	response := ""
 	if err != nil {
@@ -62,19 +62,19 @@ func slack_handler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		response = fmt.Sprintf("現金買入: %v, 現金賣出: %v\n即期買入: %v, 即期賣出: %v", currency.BuyCach, currency.SellCash, currency.BuySpot, currency.SellSpot)
 	}
-	postMessage := SlackPostMessage{}
+	postMessage := slackPostMessage{}
 	postMessage.ResponseType = "in_channel"
 	postMessage.Text = "Currency rate for: " + strings.ToUpper(r.FormValue("text"))
 
-	attachment := SlackAttachment{Text: response}
-	postMessage.Attachments = []SlackAttachment{attachment}
+	attachment := slackAttachment{Text: response}
+	postMessage.Attachments = []slackAttachment{attachment}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(postMessage)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/slack", slack_handler)
-	http.HandleFunc("/renew", renew_handler)
-	log.Fatal(http.ListenAndServe(":3030", Log(http.DefaultServeMux)))
+	http.HandleFunc("/slack", slackHandler)
+	http.HandleFunc("/renew", renewHandler)
+	log.Fatal(http.ListenAndServe(":3030", logging(http.DefaultServeMux)))
 }
