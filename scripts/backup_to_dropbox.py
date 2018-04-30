37fac538e0a3c5b76df286279a00d3ea64eeb2ed
Hank Wang <drapho@gmail.com>
2018-05-01 02:42:08 +0900

    improve exists files check

--- scripts/backup_to_dropbox.py
+++ scripts/backup_to_dropbox.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import sys
 import os
 import dropbox
+import glob
 from dropbox.files import WriteMode
 from dropbox.exceptions import ApiError, AuthError
 
@@ -10,12 +11,21 @@ from dropbox.exceptions import ApiError, AuthError
 SCRIPT_FOLDER = os.path.dirname(os.path.abspath(__file__))
 CSV_FOLDER = os.path.dirname(SCRIPT_FOLDER) + "/csvs/"
 
+# (https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/)
+dbx = dropbox.Dropbox(os.environ["DROPBOX_ACCESS_TOKEN"])
+
+# check token
+try:
+    dbx.users_get_current_account()
+except AuthError:
+    sys.exit("Invalid access token")
+
 
 def upload_file(filename):
-    with open(CSV_FOLDER + filename, "r") as f:
+    with open(CSV_FOLDER + filename, "rb") as f:
         try:
             print("Uploading file: {}".format(filename))
-            dbx.files_upload(f, "/{}".format(filename), mode=WriteMode("overwrite"))
+            dbx.files_upload(f.read(), "/{}".format(filename), mode=WriteMode("overwrite"))
         except ApiError as err:
             # This checks for the specific error where a user doesn't have
             # enough Dropbox space quota to upload this file
@@ -30,21 +40,23 @@ def upload_file(filename):
                 sys.exit()
 
 
+def list_dropbox_exists_filenames(filenames, metadata=None):
+    if metadata is not None:
+        if metadata.has_more:
+            list_folder_result = dbx.files_list_folder_continue(metadata.cursor)
+        else:
+            return filenames
+    else:
+        list_folder_result = dbx.files_list_folder("")
+
+    filenames += [entry.name for entry in list_folder_result.entries]
+    return list_dropbox_exists_filenames(filenames, list_folder_result)
+
 if __name__ == "__main__":
-    # (https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/)
-    dbx = dropbox.Dropbox(os.environ["DROPBOX_ACCESS_TOKEN"])
+    exists_files = set(list_dropbox_exists_filenames([]))
+    local_files = set(os.path.basename(f) for f in glob.glob(CSV_FOLDER + "*.csv"))
 
-    # check token
-    try:
-        dbx.users_get_current_account()
-    except AuthError:
-        sys.exit("Invalid access token")
-
-    exists_filenames = [entry.name for entry in dbx.files_list_folder("").entries]
-
-    for (dirpath, dirnames, filenames) in os.walk(CSV_FOLDER):
-        for filename in filenames:
-            name, ext = os.path.splitext(filename)
-            # only upload file with CSV extension
-            if ext == ".csv" and filename not in exists_filenames:
-                upload_file(filename)
+    waiting_to_upload_files = (local_files - exists_files)
+    print("Uploading {} files.".format(len(waiting_to_upload_files)))
+    for f in waiting_to_upload_files:
+        upload_file(f)
